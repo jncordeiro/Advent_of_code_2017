90d94eeef82fcbe1a33fd424df4b491955447063
Joao Cordeiro (qnck73) <joao.cordeiro@motorolasolutions.com>
2017-12-08 04:26:03 -0500

    Funcioning until 11

--- 3_1.c
+++ 3_1.c
@@ -17,7 +17,7 @@ int main(){
     int M[5][5];
     int x = 0;
     int y = 0;
-    int h , v;
+    int h , v, h_2, v_2;
     int iterations = 0;
     int Num;
     int arc;
@@ -39,45 +39,61 @@ int main(){
     up = left = down = 0;
     right = 1;
 
+    printf("\nright\n");
     while(Num != 25){
         while(Num <= pow(2,arc+1)+1){
+        //while(h != arc + 1 && v != arc +1){
+                if(Num == 2 || Num == 3)printf("\nh: %d,v: %d ", h, v);
+
                 M[h][v] = Num;
 
-                printf("%d\t",M[h][v]);
+                //printf("%d\t",M[h][v]);
                 if(Num == 1)arc++;
                if(up){
-                    h++;
-                    if((h-1) == arc){
+                    h--;
+                    if((h+1) == arc){
+                        printf("\nleft\n");
                         left = 1;
                         up = 0;
                     }
-                }
-               if(down){
-                    h--;
-                    if((h+1) == arc){
+                }else if(down){
+                    h++;
+                    if((h-1) == arc){
+                        printf("\nright\n");
                         right = 1;
                         down = 0;
                     }
-                }
-               if(left){
+                }else if(left){
                     v--;
                     if((v+1) == arc){
+                        printf("\ndown\n");
                         down = 1;
                         left = 0;
                     }
-                }
-               if(right){
+                }else if(right){
                     v++;
                     if(((v-1) == arc && (Num+1 !=pow(2,arc+1)+1)) || Num == pow(2,arc+1)+1){
+                        printf("\nup\n");
                         up = 1;
                         right = 0;
                     }
                 }
                 Num++;
-                if(Num <25)break;
+                v_2 = h_2 = 0;
+                while(h_2 < 5){
+                    while(v_2 < 5){
+                        printf("%d\t",M[h_2][v_2]);
+                        v_2++;
+                    }
+                    printf("\n");
+                    v_2 = 0;
+                    h_2++;
+                }
+                getchar();
+                if(Num >25)break;
        }
        arc++;
-       if(Num <25)break;
+       if(Num >25)break;
     }
     v = h = 0;
     while(h < 5){
